c067c6567245c2509df64fa64af3e43e2173f0c6
Ngô Ngọc Đức Huy <huyngo@disroot.org>
2021-02-05 17:59:36 +0700

    Add a script for generating license

---
+++ add-license.sh
@@ -0,0 +1,32 @@
+name="Ngô Ngọc Đức Huy"
+year=`date "+%Y"`
+filename=LICENSE
+if [ -z "$1" ]
+then
+	printf "Please input a license name: "
+	read license
+else
+	license=$1
+fi
+echo $license
+if license="mit"
+then
+	cat ~/utils/licenses/mit.txt | sed s/YEAR/$year/ | sed "s/NAME/$name/" > $filename
+elif license="agpl"
+then
+	cp "~/utils/licenses/agpl-3.0.txt" $filename
+elif license="gpl"
+then
+	cp "~/utils/licenses/gpl-3.0.txt" $filename
+elif license="lgpl"
+then
+	cp "~/utils/licenses/lgpl-3.0.txt" $filename
+elif license="mpl"
+then
+	cp "~/utils/licenses/mpl-2.0.txt" $filename
+elif license="apache"
+then
+	cp "~/utils/licenses/apache-2.0.txt" $filename
+else
+	echo "Invalid license name."
+fi
