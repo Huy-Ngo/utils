3848a533b9582a4989ee35ed4699e0fff2c4f203
Ngô Ngọc Đức Huy <huyngo@disroot.org>
2021-02-06 08:48:19 +0700

    Fix add license script

--- add-license.sh
+++ add-license.sh
@@ -9,24 +9,24 @@ else
 	license=$1
 fi
 echo $license
-if license="mit"
+if [ $license = "mit" ]
 then
 	cat ~/utils/licenses/mit.txt | sed s/YEAR/$year/ | sed "s/NAME/$name/" > $filename
-elif license="agpl"
+elif [ $license = "agpl" ]
 then
-	cp "~/utils/licenses/agpl-3.0.txt" $filename
-elif license="gpl"
+	cp ~/utils/licenses/agpl-3.0.txt $filename
+elif [ $license = "gpl" ]
 then
-	cp "~/utils/licenses/gpl-3.0.txt" $filename
-elif license="lgpl"
+	cp ~/utils/licenses/gpl-3.0.txt $filename
+elif [ $license = "lgpl" ]
 then
-	cp "~/utils/licenses/lgpl-3.0.txt" $filename
-elif license="mpl"
+	cp ~/utils/licenses/lgpl-3.0.txt $filename
+elif [ $license = "mpl" ]
 then
-	cp "~/utils/licenses/mpl-2.0.txt" $filename
-elif license="apache"
+	cp ~/utils/licenses/mpl-2.0.txt $filename
+elif [ $license = "apache" ]
 then
-	cp "~/utils/licenses/apache-2.0.txt" $filename
+	cp ~/utils/licenses/apache-2.0.txt $filename
 else
 	echo "Invalid license name."
 fi
